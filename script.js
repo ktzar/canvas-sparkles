428a1307caa7f296f140637dae01a12e3a8140b9
ktzar <miguelmail@gmail.com>
2012-04-25 00:31:13 +0100

    Add more physics parameters

--- script.js
+++ script.js
@@ -6,14 +6,17 @@ var animation = function () {
     var c       = canvas.getContext('2d');
 
     var config = {
-        x:      200,
-        y:      200,
-        initW:  10,
-        initH:  10,
-        decay:  5,
-        ftl:    100, //frames to live
-        rebound:true,
-        gravity: 2
+        x:          200,
+        y:          200,
+        initW:      10,
+        initH:      10,
+        decay:      5,
+        initSpeed:  20,
+        ftl:        100, //frames to live
+        rebound:    true,
+        wallAbsorb: 0.5,
+        gravity:    1,
+        shrinkRate: 0.96
     };
 
     canvas.addEventListener('mousemove', function(e) {
@@ -27,19 +30,19 @@ var animation = function () {
 
     var frame = function () {
         c.clearRect(0,0,600,600);
-        c.shadowBlur = 5;
+        c.shadowBlur = 15;
         c.fillStyle = 'rgba(0,0,0,0.75)';
 
         for (var i = 0; i < sparkles.length ; i++ ) {
             var s = sparkles[i];
 
-            var r = 200;
+            var r = 255;
             var g = 0;
             var b = 0;
             var a = (-s.decay+1) + s.decay*(s.ftl / 100);
 
-            c.fillStyle = 'rgba('+r+','+g+','+b+','+a+')';
-            c.shadowColor = 'rgba('+r+','+g+','+b+','+a+')';
+            c.fillStyle     = 'rgba('+r+','+g+','+b+','+a+')';
+            c.shadowColor   = 'rgba('+r+','+g+','+b+','+a+')';
 
             c.beginPath();
             c.arc(s.x, s.y, s.h, 0, Math.PI*2, true); 
@@ -48,17 +51,17 @@ var animation = function () {
 
             s.x += s.xSpeed;
             s.y += s.ySpeed;
-            s.h *= 0.96;
-            s.w *= 0.96;
+            s.h *= config.shrinkRate;
+            s.w *= config.shrinkRate;
             s.ySpeed += config.gravity;
             s.ftl -= 1;
 
             if ( config.rebound ) {
                 if ( s.x > width  || s.x < 0 ) {
-                    s.xSpeed *= -1;
+                    s.xSpeed *= -config.wallAbsorb;
                 }
                 if ( s.y > height  || s.y < 0 ) {
-                    s.ySpeed *= -1;
+                    s.ySpeed *= -config.wallAbsorb;
                 }
             }
 
@@ -77,15 +80,15 @@ var animation = function () {
             y       : config.y,
             h       : config.initH,
             w       : config.initW,
-            xSpeed  : parseInt(Math.random()*20)-10,
-            ySpeed  : parseInt(Math.random()*20)-10,
+            xSpeed  : (Math.random()*config.initSpeed)-config.initSpeed/2,
+            ySpeed  : (Math.random()*config.initSpeed)-config.initSpeed/2,
             ftl     : config.ftl
         };
         sparkles.push(sparkle);
     }
 
     setInterval(frame, 30);
-    setInterval(addSparkle, 30);
+    setInterval(addSparkle, 1);
     addSparkle();
 };
 window.addEventListener("load", function(){
